022fafe72b16e3a3110cf7faceb9951e89c23602
Jessica <jessicadianefletcher@gmail.com>
2024-10-13 00:42:46 -0600

    first commit

---
+++ src/Message.tsx
@@ -0,0 +1,10 @@
+// PascalCasing
+function Message() {
+    // JSX: JavaScript XML (syntax type)
+    const name = '';
+    if (name)
+        return <h1>Hello {name}</h1>;
+    return <h1>Hello World</h1>;
+}
+
+export default Message;
